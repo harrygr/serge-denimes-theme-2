facc920bcd157d6836b146888ade0a0962051506
Harry G <harrygrumbar@hotmail.com>
2014-07-06 12:04:13 +0100

    Sale prices in red

--- lib/woocommerce.php
+++ lib/woocommerce.php
@@ -30,22 +30,22 @@ function custom_woocommerce_loop_add_to_cart_link( $button ) {
 //Customise the add to cart message
 add_filter( 'wc_add_to_cart_message', 'serge_cart_message', 99);
 function serge_cart_message($message){
-		$message = str_replace('class="button', 'class="btn btn-primary', $message);
-		return $message;
+	$message = str_replace('class="button', 'class="btn btn-primary', $message);
+	return $message;
 }
 
 // Update the custom cart cookie
 // this should only ever fire on non-cached pages (so it SHOULD fire
 // whenever we add to cart / update cart / etc
 function update_cart_total_cookie() {
-    global $woocommerce;
-    $cart_total = $woocommerce->cart->get_cart_total();
-    $cart_count = $woocommerce->cart->cart_contents_count;
-    setcookie('woocommerce_cart_count', $cart_count, 0, '/');
-    setcookie('woocommerce_cart_total', $cart_total, 0, '/');
+	global $woocommerce;
+	$cart_total = $woocommerce->cart->get_cart_total();
+	$cart_count = $woocommerce->cart->cart_contents_count;
+	setcookie('woocommerce_cart_count', $cart_count, 0, '/');
+	setcookie('woocommerce_cart_total', $cart_total, 0, '/');
 }
 if(!is_admin()){
-add_action('init', 'update_cart_total_cookie');
+	add_action('init', 'update_cart_total_cookie');
 }
 
 //NUMBER OF PRODUCTS TO DISPLAY ON SHOP PAGE
@@ -54,5 +54,43 @@ function wg_view_all_products(){
 	if($_GET['view'] === 'all'){
 		return '9999';
 	}
+	return 9;
 }
- ?>
+
+
+// Secret Store
+
+add_action( 'pre_get_posts', 'serge_exclude_cat' );
+add_filter( 'woocommerce_product_categories_widget_args', 'serge_exclude_product_cat_widget' );
+
+// Hide from the main store page
+function serge_exclude_cat( $q ) 
+{
+
+	if ( ! $q->is_main_query() ) return;
+	if ( ! $q->is_post_type_archive() ) return;
+	
+	if ( ! is_admin() && is_shop() ) {
+
+		$q->set( 'tax_query', array(array(
+			'taxonomy' => 'product_cat',
+			'field' => 'slug',
+			'terms' => array( 'secret' ), // Don't display products in the secret category on the shop page
+			'operator' => 'NOT IN'
+			)));
+
+	}
+
+	remove_action( 'pre_get_posts', 'serge_exclude_cat' );
+
+}
+
+function serge_exclude_product_cat_widget( $args ) 
+{
+	$cat_id = get_term_by( 'slug', 'secret', 'product_cat');
+	// var_dump($cat_id);
+	// die();
+	$args['exclude'] = array($cat_id->term_id);
+	return $args;
+}
+?>
